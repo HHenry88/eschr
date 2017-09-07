e2395e32d71f03f07556bddd265c94a169f9d8bd
Hhenry88 <henryhan88@outlook.com>
2017-09-07 14:51:03 +0800

    Removed Vue logo and doc links. Registered  toolbar component

---
+++ src/components/toolBar.vue
@@ -0,0 +1,23 @@
+<template id="toolBar">
+    <md-toolbar class="md-dense">
+      <md-button class="md-icon-button">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title" style="flex: 1, float: left">Demo Title</h2>
+    </md-toolbar>
+</template>
+
+<script>
+  export default {
+    name: 'toolBar',
+    data(){
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
