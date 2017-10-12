d8c0ff6dbf5c6c27222368e4add222cbd5cd0565
jamie <jamie@eschr.com>
2017-10-12 17:03:56 +0800

    Convert Tag Chips to Bootstrap

--- src/components/tagChips.vue
+++ src/components/tagChips.vue
@@ -1,9 +1,22 @@
 <template lang="html">
-  <div class="tags">
-    <md-chip v-for="(keyword, index) in keywords" :key="index" md-editable v-bind:style="">
-      <span v-on:click="changeTag(keyword)">{{keyword}}</span>
-    </md-chip>
-  </div>
+  <b-container fluid>
+    <b-row align-v class="info-header">
+      <b-col col sm="1" class='icon'>
+        <img src='static/img/tags-icon.png' />
+      </b-col>
+      <b-col col sm="10">
+        <div class='info-title'>Things</div>
+	    </b-col>
+    </b-row>
+    <b-row class="justify-content-md-center"> 
+      <b-col col lg="1"></b-col>
+      <b-col cols="11">
+        <div v-for="(keyword, index) in keywords" :key="index" v-bind:style="">
+          <div v-on:click="changeTag(keyword)" class="tag blue">{{keyword}}</div>
+        </div>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -50,4 +63,39 @@ export default {
     margin: 2px;
     font-size: 2em;
   }
+  .tag, .color {
+  	font-size:2em;
+  	text-transform:uppercase;
+  	float:left;
+  	padding: 5px 8px 3px 8px;
+  	margin: 0px 5px 5px 0px;
+  	background: #FFF;
+  	border:1px solid #006FFF;
+  	font-size:.75em;
+  	color:#006FFF;
+	}
+
+  .blue {
+  	background: #006FFF;
+  	border:1px solid #006FFF;
+  	color:#fff;
+	}
+	.icon img {
+  	width:100%;
+  	max-width:1.5em;
+  	float:right;
+	}
+	.info-title {
+  	font-weight:bold;
+  	font-size:1em;
+  	color:#2a2a2a;
+  	text-align: left;
+	}
+	.info-header {
+  	height:50px;
+	}
+	.info-subtitle,.info-body {
+    font-size:.80em;
+    color: #ccc;
+	}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import Router from 'vue-router'
 import Main from '@/components/mainpage'
 import singleItem from '@/views/singleItem'
 import demo from '@/views/demo'
 import demoDrillDown from '@/views/demoDrillDown'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.use(Router)
+Vue.use(BootstrapVue);
 
 export default new Router({
   routes: [
