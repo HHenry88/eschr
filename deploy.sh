dd69b589df4a0d2896df040e0f5494420c55e97d
jamie <jamie@eschr.com>
2017-10-26 10:53:50 +0800

    Update deploy to reference JS files local to deployment dir. TODO: This doesn't scale. We need  to include only local values here - and host the main JS/CSS in root.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-aws s3 cp --recursive dist/ s3://search.miro.io/
-aws cloudfront create-invalidation --distribution-id E3494MQB8BK75B --paths /
+aws s3 cp --recursive dist/ s3://search.miro.io/travel/
+aws cloudfront create-invalidation --distribution-id E3494MQB8BK75B --paths /travel/
